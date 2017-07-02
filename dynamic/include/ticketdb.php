6772f6d76d71e7e808c9261b403828558fe71260
Florian Pesth <fpesth@gmx.de>
2017-07-02 16:09:20 +0200

    [Registration] Add simple database access for generation of tickets

--- dynamic/include/ticketdb.php
+++ dynamic/include/ticketdb.php
@@ -1,28 +1,68 @@
 <?php
-# Class for accesing the ticket database
+# Class for accesing the ticket database in a more abstract way
 namespace Jonglaria;
+
+require_once("/is/htdocs/wp1110266_HJD5OK7U68/jonglariahidden/config/mexicon/config.php");
+require_once("/is/htdocs/wp1110266_HJD5OK7U68/jonglariahidden/dynamic/convention/database.inc.php");
 
 class TicketDatabase
 {
+	private $current_id = 0;
+	private $id_info;
+	private $db = null;
+	private $caregiver;
+	public function __construct()
+	{
+		$this->db = new \Database("db1110266-jonglaria");
+	}
+	private function refreshInformation($id)
+	{
+		if(($id != $current_id) || ($current_id == 0))
+		{
+			$current_id = $id;
+			$res = $this->db->query("SELECT * FROM `person` JOIN `convention` ON `person`.`id` = `convention`.`id`;");
+			$this->id_info = $this->db->fetch_assoc($res);
+			$res_cg = $this->db->query("SELECT * FROM `caregiver` WHERE `id` = '".$id."'");
+			$this->caregiver = $this->db->fetch_assoc($res_cg);
+			if(!$this->caregiver) $this->caregiver=null;
+		}
+	}
 	public function getFirstName($id)
 	{
-		return "Max";
+		$this->refreshInformation($id);	
+		return $this->id_info['prename'];
 	}
 	public function getFamilyName($id)
 	{
-		return "Mustermann";
+		$this->refreshInformation($id);	
+		return $this->id_info['surname'];
 	}
 	public function getBirthDate($id)
 	{
-		return "1.4.1990";
+		$this->refreshInformation($id);	
+		// Format birthday nicely:
+		$birthday = new \DateTime($this->id_info['birthday'], new \DateTimeZone('UTC'));
+		$birthday->setTimezone(new \DateTimeZone('Europe/Berlin'));
+		$birthday = $birthday->format("d.m.Y");
+
+		return $birthday;
 	}
 	public function getSuperVisor($id)
 	{
-		return "Maria Mustermutter";
+		$this->refreshInformation($id);	
+		if($this->caregiver != null)
+		{
+			return $this->caregiver;
+		}
+		else
+		{
+			return "";
+		}
 	}
 	public function getEmail($id)
 	{
-		return "fpesth@gmx.de";
+		$this->refreshInformation($id);	
+		return $this->id_info['email'];
 	}
 }
 ?>
