f9ff85d0d525743d690c830c0c83a525f524055e
Florian Pesth <fpesth@gmx.de>
2018-06-05 19:38:43 +0200

    [Mexicon] Add changes not commited to clean local copy

--- mexicon/inc/createTicket.inc.php
+++ mexicon/inc/createTicket.inc.php
@@ -1,28 +1,17 @@
 <?php // createTicket.inc.php
-// this function needs a 'tickets/' subdirectory in the BASEDIR
-// with ticket pdf creation files 
+// Not strictly needed anymore because function could be called directly instead but whatever
+
+require_once("/is/htdocs/wp1110266_HJD5OK7U68/jonglariahidden/dynamic/ticket.php");
 
 function createConventionTicket($id,$DB) {
-  $res = $DB->query("SELECT * FROM `person` JOIN `convention` ON `person`.`id` = `convention`.`id` WHERE id=".$id.";");
-  $person = $DB->fetch_assoc($res);
-  $regtime = new DateTime($person['regtime'], new DateTimeZone('UTC'));
-  $regtime->setTimezone(new DateTimeZone('Europe/Berlin'));
-  $regtime = $regtime->format("d.m.Y");
-
-  exec("LD_LIBRARY_PATH=$(pwd):".'$LD_LIBRARY_PATH'." ".$_SERVER['DOCUMENT_ROOT'].BASEDIR."/tickets/example1"
-      ." -1".$person['prename']
-      ." -2".$person['surname']
-      ." -b".$regtime
-      ." -s\"Matilda Mustermutter\""
-      ." -i\"http://jonglaria.org/reg/f458c955\""
-      ." > ".$person['id'].".ps", $ps_output, $retval);
-  exec("TEMP=".$_SERVER['DOCUMENT_ROOT'].BASEDIR." gs"
-      ." -dBATCH -dNOPAUSE -sPAPERSIZE=a4 -dNOEPS -dAutoFilterColorImages=false"
-      ." -dColorImageFilter=/FlateEncode -sDEVICE=pdfwrite"
-      ." -o ".$person['id'].".pdf"
-      ." -f ".$person['id'].".ps"
-      ." -c \"[ /Author (Jonglaria e.V.) /Subject (Electronic Ticket)"
-      ." /Keywords (Convention, Tuebingen, 2017, Juggling, Jonglaria) /DOCINFO pdfmark\"", $ps_output, $retval);
+	$ticket = new Jonglaria\Ticket;
+	$ticket->sendElectronicTicket($id);
 }
+
+function createGalaTicket($id,$DB) {
+	$ticket = new Jonglaria\Ticket;
+	$ticket->sendElectronicTicketGala($id);
+}
+
 
 ?>
